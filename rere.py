3d4bf698c487b4b7c203f5d19b8c065fac9ae454
Mark Karunos <mark.karynos@gmail.com>
2024-11-23 01:09:30 +0300

    penis11111

--- rere.py
+++ rere.py
@@ -5,26 +5,31 @@ async def main():
     chat_url = 'https://web.telegram.org/a/#7631205793'
     
     async with async_playwright() as p:
-        browser = await p.chromium.launch_persistent_context(headless=True, user_data_dir='./userdata')
-        page = await browser.new_page()
+        browser = await p.chromium.launch(headless=False)
+        context = await browser.new_context(storage_state="state.json")
+        page = await context.new_page()
         await page.goto(chat_url)
         
-        
+        # 
         playbtn = page.locator('//*[@id="MiddleColumn"]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div/button[1]')
+        await asyncio.sleep(10)
         await expect(playbtn).to_be_visible()
         await playbtn.click()
-        
+        confirm = page.locator('//*[@id="portals"]/div[2]/div/div/div[2]/div[2]/div/button[1]')
+        await expect(confirm).to_be_visible()
+        await confirm.click()
         # Wait for the iframe to be available
         await page.wait_for_selector('iframe')
         gframe = page.frame_locator('iframe')
         
         # Locate buttons inside the frame
         getbutn = gframe.locator('//*[@id="ui-bottom"]/a[2]')
-        exitbtn = page.locator('//*[@id="portals"]/div[1]/div/div/div[2]/div[1]/div/button[1]')
+        # exitbtn = page.locator('//*[@id="portals"]/div[1]/div/div/div[2]/div[1]/div/button[1]')
         await expect(getbutn).to_be_visible()
         await getbutn.click()
-        await exitbtn.click()
-        await asyncio.sleep(1)
+        # await exitbtn.click()
+        await browser.close()
+        
 
 if __name__ == '__main__':
     asyncio.run(main())
